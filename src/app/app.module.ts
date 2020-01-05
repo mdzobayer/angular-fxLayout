9f7d6c80bb382c526f3f8f6262047c6907eec61f
mdzobayer <mdzobayer@rocketmail.com>
2020-01-05 16:35:43 +0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CardComponentComponent } from './card-component/card-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/card-component/card-component.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  template: `
+    <div>Card: {{name}}</div>
+  `,
+  styles: [`
+    :host {
+      display: block;
+      padding: 32px;
+      border: 1px solid black;
+      border-radius: 8px;
+    }
+  `]
+})
+export class CardComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
